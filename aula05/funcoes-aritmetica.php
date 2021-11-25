63a961da377ce26771a7171e4ecc299075a34730
regispicaz <registavares@protonmail.com>
2021-11-24 23:03:39 -0300

    conclusão do material aula 05

--- aula05/funcoes-aritmetica.php
+++ aula05/funcoes-aritmetica.php
@@ -11,7 +11,15 @@
     <div>
         <?php
             // Funções aritméticas
-            
+            $v1 = $_GET ["x"]; // Recebe o valor da variável x via url
+            $v2 = $_GET ["y"]; // Recebe o valor da variável y via url
+            echo "Valores recebidos : $v1 e $v2 </br> ";
+            echo "O valor absouto de $v2 é ".abs($v2); // Função abs()
+            echo "</br>O valor de $v1<sup>$v2</sup> é ".pow($v1, $v2); // Função pow()
+            echo "</br>A raiz de $v1 é ".sqrt($v1); // Função sqrt()
+            echo "</br>O valor arredondado de $v2 é ".round($v2); // Função round() | ceil() arrendonda para cima | floor() arrendonda para baixo
+            echo "</br>A parte inteira de $v2 é ".intval($v2); // Função intval() 
+            echo "</br>O valor de $v1 em moeda é R$ ".number_format($v1, 2, ",", "."); // Função number_format()
             
         ?>
     </div>
